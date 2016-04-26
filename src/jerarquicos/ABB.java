8b85a652b6bcbae7a07803b384d5c65385539d31
Angelotas <angel.martin.bartolome.95@gmail.com>
2016-04-26 20:31:14 +0200

    Primeros cambios practica árboles, este será el branch master, cuando comiencen los cambios complicados se creará un nuevo branch

--- src/jerarquicos/ABB.java
+++ src/jerarquicos/ABB.java
@@ -19,4 +19,93 @@ public class ABB <E extends Comparable<E>>{
     public ABB(E x){
         raiz = new NodoABB<E>(x);
     }
+    
+    public int tamanyo(){
+        return 1;
+    }
+    public boolean esVacio(){
+        return true;
+    }
+    /*---------------------------------------------------------------------*/
+    /*---------------------------------------------------------------------*/
+    public int altura(){
+        return 1;
+    }
+    protected int altura(NodoABB<E> actual){
+        return 1;
+    }
+    /*---------------------------------------------------------------------*/
+    /*---------------------------------------------------------------------*/
+    public void insertarSinDuplicados(E x){
+        //insertar a partir de la raiz del árbol
+        NodoABB<E> n= new NodoABB<E>(x);
+        
+    }
+    protected NodoABB<E> insertarSinDuplicados(E x, NodoABB<E> actual){
+        //insertar sin duplicado a partir de un determinado nodo
+        return null;
+    }
+    /*---------------------------------------------------------------------*/
+    /*---------------------------------------------------------------------*/
+    public void insertarConDuplicados(E x){
+        //insertar con duplicado desde la raiz del árbol
+        NodoABB<E> n= new NodoABB<E>(x);
+        
+    }
+    protected NodoABB<E> insertarConDuplicados(E x, NodoABB<E> actual){
+        //insertar con duplicado a partir de un determinado nodo
+        return null;
+    }
+    /*---------------------------------------------------------------------*/
+    /*---------------------------------------------------------------------*/
+    public void actualizar(E x){
+        //Si x no esta se inserta, pero si esta, el árbol actualiza el nodo con el nuevo valor
+        NodoABB<E> n= new NodoABB<E>(x);
+    }
+    protected NodoABB<E> actualizar(E x, NodoABB<E> actual){
+        //Si x no esta se inserta, pero si esta, el árbol actualiza el nodo con el nuevo valor
+        NodoABB<E> n= new NodoABB<E>(x);
+        return null;
+    }
+    /*---------------------------------------------------------------------*/
+    /*---------------------------------------------------------------------*/
+    
+    public E recuperar(E x){
+        return null;
+    }
+    protected NodoABB<E> recuperar (E x, NodoABB<E> n){
+        return null;
+    }
+    /*---------------------------------------------------------------------*/
+    /*---------------------------------------------------------------------*/
+    
+    public E recuperarMin(){
+        //devuelve el dato de tipo E mas pequeño del árbol
+        return null;
+    }
+    protected NodoABB<E> recuperarMin(NodoABB<E> actual){
+        //devuelve el dato de tipo E mas pequeño del nodo que se le pasa por referencia
+        return null;
+    }
+    /*---------------------------------------------------------------------*/
+    /*---------------------------------------------------------------------*/
+    
+    public void eliminar(E x){
+        
+    }
+    protected NodoABB<E> eliminar (E x, NodoABB<E> actual){
+        return null;
+    }
+    /*---------------------------------------------------------------------*/
+    /*---------------------------------------------------------------------*/
+    
+    public void eliminarMin(){
+        
+    }
+    protected NodoABB<E> eliminarMin(NodoABB<E> actual){
+        return null;
+    }
+    /*---------------------------------------------------------------------*/
+    /*---------------------------------------------------------------------*/
+    
 }
---
+++ src/jerarquicos/Medicamento.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jerarquicos;
+
+/**
+ *
+ * @author Ángel
+ */
+public class Medicamento implements Comparable<Medicamento>{
+
+    private String codigoB;
+    private String nombre;
+    private String laboratorio;
+    private float precio;
+    private float porcentajeSS;
+    
+    public Medicamento (String c, String nom, String lab, float pre, float por){
+        this.codigoB=c;
+        this.nombre=nom;
+        this.laboratorio=lab;
+        this.precio=pre;
+        this.porcentajeSS=por;
+    }
+    
+    @Override
+    public int compareTo(Medicamento m) {
+        
+        if (this.codigoB.compareTo(m.getCodigoB()) > 0){
+            return 0; //este codigo es mayor que el que le pasamos por referencia
+        }
+        else if (this.codigoB.compareTo(m.getCodigoB()) < 0){
+            return 1; //este codigo es menor que el que le pasamos por referencia
+        }
+        else{
+            return 2;  //ambos códigos son iguales
+        }
+    }
+    
+    public String getCodigoB(){
+        return this.codigoB;
+    }
+    
+    public String toString(){
+        return "\n\t..::MEDICAMENTO::..\n-->Código de barras: "+this.codigoB+"\n-->Nombre: "+this.nombre+"\nLaboratorio: "
+                +this.laboratorio+"\n-->Precio: "+this.precio+"\nPorcentaje SS: "+this.porcentajeSS+"\n ";
+    }
+    
+}
